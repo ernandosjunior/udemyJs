5cece6f9722372a51e08fd1e5e901603a7eef1ee
Ernando S Junior <ernandosjunior@hotmail.com>
2022-11-26 11:57:04 -0300

    add section jsBasic with class abstract

---
+++ jsBasico/objectMath.js
@@ -0,0 +1,10 @@
+let num1 = 9.49
+
+console.log(`arredonda para cima ${Math.ceil(num1)}`)
+console.log(`arredonda para baixo ${Math.floor(num1)}`)
+console.log(`arredonda para o mais próximo (${num1}) ${Math.round(num1)}`)
+console.log(`arredonda para o mais próximo (9.51) ${Math.round(9.51)}`)
+console.log(`gerar número aleatório ${Math.random()}`)
+
+console.log(`retorna o maior número ${Math.max(1,2,3,245,2345,6,24,413,6,54,767,3,56,345,63,546)}`)
+console.log(`retorna o menor número ${Math.min(134,13,2,25,62,4,5234,47,67,46,56,243,52,5674,68)}`)
