7ac74d5c3f60059880a569433581958bcf789ea5
Ernando Junior <ernandosjunior@hotmail.com>
2023-06-16 16:12:02 -0300

    calculadora com function constructor

---
+++ projetos/calculadora function Constructors/script.js
@@ -0,0 +1,59 @@
+function Calculadora(){
+  this.display = document.querySelector('.display');
+  
+  this.iniciar = function() {
+    this.cliqueBotoes();
+    this.pressionaEnter();
+  };
+  this.pressionaEnter = function() {
+    this.display.addEventListener('keyup', e => {
+      if( e.keyCode === 13){
+        this.fazerConta();
+      }
+    })
+  };
+  this.cliqueBotoes = function(){
+    document.addEventListener('click', function(e){
+      const el = e.target;
+
+      if(el.classList.contains('btn-num')){
+        this.btnParaDisplay(el.innerHTML);
+      }
+      if(el.classList.contains('btn-del')){
+        this.btnDelete();
+      }
+      if(el.classList.contains('btn-clear')){
+        this.btnLimpar();
+      }
+      if(el.classList.contains('btn-eq')){
+        this.fazerConta();
+      }
+      this.display.focus();
+    }.bind(this));
+  };
+  this.btnParaDisplay = function(valor) {
+    this.display.value += valor
+  };
+
+  this.btnDelete = function() {
+    this.display.value = this.display.value.slice(0,-1);
+  };
+
+  this.btnLimpar = function() {
+    this.display.value = ''
+  };
+
+  this.fazerConta = function(){
+    try {
+      this.display.value = eval(this.display.value)  
+    } catch (error) {
+      alert("conta invalida");
+      console.log(error)
+      return
+    };
+  };
+
+
+}
+const calculadora = new Calculadora();
+calculadora.iniciar();
