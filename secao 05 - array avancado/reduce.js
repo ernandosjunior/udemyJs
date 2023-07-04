4c566d09707e7434c02e5872fd71bb1f6f05b057
Ernando Junior <ernandosjunior@hotmail.com>
2023-07-04 20:53:12 -0300

    adicionando caso de erro por não setar valor inicial do acumulador

--- secao 05 - array avancado/reduce.js
+++ secao 05 - array avancado/reduce.js
@@ -1,7 +1,15 @@
-const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const array = [2,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 const total = array.reduce(function (acumulador,valor) {
   acumulador+= valor;
   return acumulador;
 }, 120);//valor inicial do acumulador
 
 console.log(total)
+
+const somaImpares = array.reduce(function(acumulador,valor){
+  if(valor % 2 !== 0){
+    acumulador+= valor
+  }
+  return acumulador
+}, 0)//caso não for setado o valor inicial do acumulador e o primeiro termo do array não passar na condição, o retorno estará incorreto.
+console.log(`soma dos impares é igual a ${somaImpares}`)
