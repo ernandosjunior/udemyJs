740168944902c2594458941fa2ae0c44fdfa1bc8
Ernando Junior <ernandosjunior@hotmail.com>
2023-07-04 20:44:26 -0300

    funcao reduce

---
+++ secao 05 - array avancado/reduce.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const total = array.reduce(function (acumulador,valor) {
+  acumulador+= valor;
+  return acumulador;
+}, 120);//valor inicial do acumulador
+
+console.log(total)
